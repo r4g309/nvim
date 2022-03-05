0807ed4b66d36e685100ec775151ad5a131c1d7f
r4g3 <bscortes94@ucatolica.edu.co>
2022-03-05 13:40:23 -0500

    Added vim surround

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -63,7 +63,7 @@ return packer.startup(function(use)
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   --use "lunarvim/darkplus.nvim"
   use 'olimorris/onedarkpro.nvim'
-
+  use 'https://tpope.io/vim/surround.git'
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
